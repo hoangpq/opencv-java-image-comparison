4089f70b8c1251035da7c69af550c2194197d2ad
Hoang Phan <hoangpq@users.noreply.github.com>
2020-06-17 16:41:21 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
   -D CMAKE_INSTALL_PREFIX=/usr/local \
   -D INSTALL_PYTHON_EXAMPLES=ON \
   -D INSTALL_C_EXAMPLES=ON ..
-make -j$(nproc)
+make -j4
 
 SHARED_PATH=$(find "$PWD" . -name *.dylib | awk 'NR==1{print $1}')
 SHARED_PATH_SO=$(echo $SHARED_PATH | sed -e "s/dylib/so/g")
