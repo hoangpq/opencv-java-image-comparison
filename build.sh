6d2ef862f0247f87b3df779649098b289d41d666
hoangpq <pquochoang2007@gmail.com>
2020-06-17 21:55:20 +0700

    Update build script

--- build.sh
+++ build.sh
@@ -5,13 +5,10 @@ tar -xvf ./opencv.tar.gz -C ./
 
 cd "opencv-$VERSION"
 mkdir build && cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-  -D CMAKE_INSTALL_PREFIX=/usr/local \
-  -D INSTALL_PYTHON_EXAMPLES=ON \
-  -D INSTALL_C_EXAMPLES=ON ..
+cmake -D -DBUILD_SHARED_LIBS=OFF ..
 make -j4
 
-SHARED_PATH=$(find "$PWD" . -name *.dylib | awk 'NR==1{print $1}')
+SHARED_PATH=$(find "$PWD" -name *java*.dylib | awk 'NR==1{print $1}')
 SHARED_PATH_SO=$(echo $SHARED_PATH | sed -e "s/dylib/so/g")
 cp $SHARED_PATH $SHARED_PATH_SO
 
